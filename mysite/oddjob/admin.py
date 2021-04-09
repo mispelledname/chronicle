b345696889f74238a7750a8d1855322708164afe
Sean Tse <seant@openpay.com.au>
2021-04-09 12:37:00 +1000

    database objects created

--- mysite/oddjob/admin.py
+++ mysite/oddjob/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Job
+from .models import User
+from .models import Tag
+
+admin.site.register(Job)
+admin.site.register(User)
+admin.site.register(Tag)
--- mysite/oddjob/models.py
+++ mysite/oddjob/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+
+class User(models.Model):
+    name = models.CharField(max_length=25)
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=50)
+    def __str__(self):
+        return self.tag
+
+class Job(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=500)
+    originUser = models.ForeignKey(User, on_delete=models.CASCADE, related_name='originUser')
+    contributors = models.ManyToManyField(User, related_name='contributors')
+    datePosted = models.DateTimeField()
+    dateDue = models.DateTimeField()
+    numHelperRequested = models.IntegerField()
+    tags = models.ManyToManyField(Tag)
+    taskDuration = models.DurationField()
+    def __str__(self):
+        return self.title
