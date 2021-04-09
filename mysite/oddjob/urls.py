0220d6541d8ab8741c64ed5c07522c60c908fc09
James Pan <jpan0022@student.monash.edu>
2021-04-09 15:39:09 +1000

    Sean changes

--- mysite/oddjob/urls.py
+++ mysite/oddjob/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:job_id>/', views.detail, name='detail' )
+    path('<int:job_id>/', views.detail, name='detail' ),
+    path('user/<int:user_id>/', views.user, name='user'),
+    path('newpost', views.post, name = 'new_post')
 ]
