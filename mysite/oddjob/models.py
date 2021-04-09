cba6779922c689a942a2c1c4d7f3301854b5ccf3
Sean Tse <seant@openpay.com.au>
2021-04-09 15:30:13 +1000

    added skeleton page to create job post

--- mysite/oddjob/models.py
+++ mysite/oddjob/models.py
@@ -16,14 +16,14 @@ class Tag(models.Model):
         return self.tag
 
 class Job(models.Model):
-    title = models.CharField(max_length=150)
-    description = models.CharField(max_length=500)
-    originUser = models.ForeignKey(User, on_delete=models.CASCADE, related_name='originUser')
-    contributors = models.ManyToManyField(User, related_name='contributors')
-    datePosted = models.DateTimeField()
-    dateDue = models.DateTimeField()
-    numHelperRequested = models.IntegerField()
-    tags = models.ManyToManyField(Tag)
-    taskDuration = models.DurationField()
+    title = models.CharField(max_length=150, null=True, blank=True)
+    description = models.CharField(max_length=500, null=True, blank=True)
+    originUser = models.ForeignKey(User, on_delete=models.CASCADE, related_name='originUser', null=True, blank=True)
+    contributors = models.ManyToManyField(User, related_name='contributors', null=True, blank=True)
+    datePosted = models.DateTimeField(null=True, blank=True)
+    dateDue = models.DateTimeField(null=True, blank=True)
+    numHelperRequested = models.IntegerField(null=True, blank=True)
+    tags = models.ManyToManyField(Tag, null=True, blank=True)
+    taskDuration = models.DurationField(null=True, blank=True)
     def __str__(self):
         return self.title
--- mysite/oddjob/views.py
+++ mysite/oddjob/views.py
@@ -1,13 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 
-from .models import Job
+from .models import Job, User, Tag
+import datetime
 
 def index(request):
     return HttpResponse("Hello, world. You're at the oddjob index.")
 
 def detail(request, job_id):
-    context = {'job': Job.objects.get(id=job_id)}
+    try:
+        context = {'job': Job.objects.get(id=job_id)}
+    except:
+        raise Http404("Job does not exist.")
     return render(request, 'oddjob/detail.html', context)
+
+def user(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    return render(request, 'oddjob/user.html', context={'user':user})
+
+def post(request):
+    if request.method == 'POST':
+        [h, m] = list(map(int, request.POST.get('duration').split(':')))
+        job = Job(title=request.POST.get('title'),
+        description=request.POST.get('description'),
+        taskDuration = datetime.timedelta(hours=h, minutes=m ),
+        datePosted = datetime.datetime.now(), 
+        dateDue = request.POST.get('datedue'),
+        numHelperRequested = request.POST.get('numHelpers'))
+        job.save()
+    return render(request, 'oddjob/newpost.html')
