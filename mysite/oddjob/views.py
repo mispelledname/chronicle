2d37ddc544bc66f5205aa222237cf6da41f07c0b
Sean Tse <seant@openpay.com.au>
2021-04-09 13:17:01 +1000

    added template for detail

--- mysite/oddjob/views.py
+++ mysite/oddjob/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 
+from .models import Job
 
 def index(request):
     return HttpResponse("Hello, world. You're at the oddjob index.")
+
+def detail(request, job_id):
+    template = loader.get_template('oddjob/detail.html')
+    context = {'job': Job.objects.get(id=job_id)}
+    return HttpResponse(template.render(context, request))
